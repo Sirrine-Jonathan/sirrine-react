bbcaaf14bc6f10e18230519860483ed8194826e1
jonsirrine <sir15004@byui.edu>
2018-04-07 16:23:26 -0700

    removed single quotes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Jonathan\'s React Demo</h1>
+          <h1 className="App-title">Welcome to Jonathan's React Demo</h1>
 		 <h3>This project was bootstrapped with <a href="https://github.com/facebook/create-react-app">Create React App</a>.<br />
 		 This sets up the environment so react can work</h3>
         </header>
@@ -16,7 +16,7 @@ class App extends Component {
 		 After the environment is set up, which includes: 
 		 <ul>
 			<li>installing node.js</li>
-			<li>running \'create react app\' to set up React, JSX, ES6, and Flow syntax support</li>
+			<li>running create react app to set up React, JSX, ES6, and Flow syntax support</li>
 			<li>and hosting the project on heroku</li>
 		 </ul>
           All you have to do is edit <code>src/App.js</code> to customize the project.
